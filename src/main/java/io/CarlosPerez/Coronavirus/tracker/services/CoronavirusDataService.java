f3b39b9711e342f3e1c1442df8398ae2aafa46f1
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-07 23:09:19 +0200

    feat: http connection added. Database connection ok

--- src/main/java/io/CarlosPerez/Coronavirus/tracker/services/CoronavirusDataService.java
+++ src/main/java/io/CarlosPerez/Coronavirus/tracker/services/CoronavirusDataService.java
@@ -1,20 +1,27 @@
 package io.CarlosPerez.Coronavirus.tracker.services;
 
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 
-
+@Service
 public class CoronavirusDataService {
 
     private static String VIRUS_DATA_URL = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv";
-    
-    public void fetchVirusData() {
+
+    @PostConstruct
+    public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.body());
     }
 }
