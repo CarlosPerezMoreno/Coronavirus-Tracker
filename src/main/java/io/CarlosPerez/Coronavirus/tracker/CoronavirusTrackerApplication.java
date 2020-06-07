ba5dad320606dc5aec1ead5fdfc642a1f0ba709f
Carlos <carlos.perez.moreno.02@gmail.com>
2020-06-07 18:50:40 +0200

    feat: First commit. Enviroment set up

---
+++ src/main/java/io/CarlosPerez/Coronavirus/tracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.CarlosPerez.Coronavirus.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/io/CarlosPerez/Coronavirus/tracker/services/CoronavirusDataService.java
@@ -0,0 +1,20 @@
+package io.CarlosPerez.Coronavirus.tracker.services;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+
+
+public class CoronavirusDataService {
+
+    private static String VIRUS_DATA_URL = "https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv";
+    
+    public void fetchVirusData() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+                .build();
+        HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
